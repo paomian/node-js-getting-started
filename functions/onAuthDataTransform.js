50d62d889540b1133c35686c08187bce8679e57b
paomian <qtang@leancloud.rocks>
2021-07-15 15:42:22 +0800

    x

--- functions/onAuthDataTransform.js
+++ functions/onAuthDataTransform.js
@@ -2,4 +2,5 @@ var AV = require('leanengine');
 
 AV.Cloud.onAuthDataTransform((request) => {
     console.log("onAuthDataTransform", request);
+    return { authData: { woshishei: { uid: "123" } } }
 });
